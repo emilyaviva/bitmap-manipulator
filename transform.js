b24be6d89e22f8ed4652d80e4478a22ff4877cd8
Emily Kapor <emily@emilyaviva.com>
2015-07-12 13:50:18 -0700

    wrote func for inverting non-palette bitmaps

--- transform.js
+++ transform.js
@@ -1,14 +1,3 @@
-var fs = require('fs');
+var invert = require('./lib/invert.js').invertBitmap;
 
-function getBitmap(req) {
-  fs.readFile(req, function(err, data) {
-    // make sure we have a bitmap file
-    if (data.toString('ascii', 0, 2) !== 'BM') {
-      throw new Error('Not a bitmap image file');
-    }
-    var startOffset = (data.readUInt8(0x0a));
-    console.log('image data starts at byte: ' + startOffset);
-  });
-}
-
-getBitmap('./img/palette-bitmap.bmp');
+invert('./img/non-palette-bitmap.bmp', './img/non-palette-bitmap-inverted.bmp');
